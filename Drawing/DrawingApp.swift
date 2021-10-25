9f2452f2ab9025967bd35e868d7f2656db441561
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-25 10:54:11 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Giovanni Gaffé on 2021/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
