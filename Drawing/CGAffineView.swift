978cecc814746c19df68bc22f62c8fbd75962031
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-26 12:55:15 +0800

    set some state

--- Drawing/CGAffineView.swift
+++ Drawing/CGAffineView.swift
@@ -31,6 +31,9 @@ struct Flower: Shape {
     
 }
 struct CGAffineView: View {
+    @State private var petalOffSet = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
