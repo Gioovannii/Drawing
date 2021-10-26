11a955b8d89c0d3dd7779366efee0e1f251c2ffc
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-26 12:28:42 +0800

    Add cgAffine file

---
+++ Drawing/CGAffineView.swift
@@ -0,0 +1,20 @@
+//
+//  CGAffineView.swift
+//  Drawing
+//
+//  Created by Giovanni Gaffé on 2021/10/26.
+//
+
+import SwiftUI
+
+struct CGAffineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CGAffineView_Previews: PreviewProvider {
+    static var previews: some View {
+        CGAffineView()
+    }
+}
