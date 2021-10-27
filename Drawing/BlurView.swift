af4a58c9628e56d3d34f3e647c443bd1d8691d89
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-27 09:42:16 +0800

    New file blur

---
+++ Drawing/BlurView.swift
@@ -0,0 +1,20 @@
+//
+//  BlurView.swift
+//  Drawing
+//
+//  Created by Giovanni Gaffé on 2021/10/27.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BlurView_Previews: PreviewProvider {
+    static var previews: some View {
+        BlurView()
+    }
+}
