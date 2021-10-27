11d60971b798ba0fa7478c6085377a76da354cfc
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-27 10:40:16 +0800

    CheckerBoard with smooth animation

---
+++ Drawing/ComplexShapesView.swift
@@ -0,0 +1,65 @@
+//
+//  ComplexShapesView.swift
+//  Drawing
+//
+//  Created by Giovanni Gaffé on 2021/10/27.
+//
+
+import SwiftUI
+
+struct checkerBoeard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    public var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            self.rows = Int(newValue.first)
+            self.columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / CGFloat(rows)
+        let columnSize = rect.width / CGFloat(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * CGFloat(column)
+                    let startY = rowSize * CGFloat(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        return path
+    }
+}
+
+struct ComplexShapesView: View {
+        @State private var rows = 4
+        @State private var columns = 4
+
+    var body: some View {
+        checkerBoeard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    self.rows = 8
+                    self.columns = 16
+                }
+            }
+    }
+}
+
+struct ComplexShapesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComplexShapesView()
+    }
+}
