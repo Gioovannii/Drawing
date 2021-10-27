e3a8a08a0024909199fe70401d26093a525d514d
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-10-27 10:21:46 +0800

    Draw trapezoid

---
+++ Drawing/AnimableDataView.swift
@@ -0,0 +1,50 @@
+//
+//  AnimableDataView.swift
+//  Drawing
+//
+//  Created by Giovanni Gaffé on 2021/10/27.
+//
+
+import SwiftUI
+
+struct Trapezoid: Shape {
+    var insetAmount: CGFloat
+    
+    var animatableData: CGFloat {
+        get { insetAmount }
+        set { self.insetAmount = newValue }
+    }
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: 0, y: rect.maxY))
+        path.addLine(to: CGPoint(x: insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - insetAmount, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+        
+        return path
+        
+    }
+}
+
+struct AnimableDataView: View {
+    @State private var insetAmount: CGFloat = 0.0
+    var body: some View {
+        VStack {
+            Trapezoid(insetAmount: insetAmount)
+                .frame(width: 200, height: 100)
+                .onTapGesture {
+                    withAnimation {
+                        self.insetAmount = CGFloat.random(in: 10...90)
+                    }
+                }
+        }
+    }
+}
+
+struct AnimableDataView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimableDataView()
+    }
+}
